4d8dc3860c7fa1c48ad05cb7b1e267db74c688ee
github username <github email address>
2019-07-19 14:17:11 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+// MAP
+const mapToNegativize = (array) => {
+    let newArray = []
+    for(let i = 0; i<array.length; i++){
+        newArray.push(0 - array[i])
+    }
+    return(newArray)
+}
+
+const mapToNoChange = (array) => {
+    let newArray = []
+    for(let i=0; i<array.length; i++){
+        newArray.push(array[i])
+    }
+    return(newArray)
+}
+
+const mapToDouble = (array) => {
+    let newArray = []
+    for(let i=0; i<array.length; i++){
+        newArray.push(array[i] * 2)
+    }
+    return newArray
+}
+
+const mapToSquare = (array) => {
+    let newArray = []
+    for(let i=0; i<array.length; i++){
+        newArray.push(array[i] * array[i])
+    }
+    return newArray
+}
+
+// REDUCE
+const reduceToTotal = (sourceArray, startingPoint = 0) => {
+    let value = startingPoint
+    for(let i=0; i<sourceArray.length; i++){
+        value += sourceArray[i]
+    }
+    return(value)
+}
+
+const reduceToAllTrue = (sourceArray) => {
+    for(let i=0; i<sourceArray.length; i++){
+        if (!sourceArray[i]) return false
+    }
+    return true
+}
+
+const reduceToAnyTrue = (sourceArray) => {
+    for(let i=0; i<sourceArray.length; i++){
+        if (sourceArray[i]) return true
+    }
+    return false
+
+    
+}
