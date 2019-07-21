33d7ab9a9d3ddeb90152a152562db4c664b01d81
Federica Macchi <federica.macchi3@gmail.com>
2019-07-21 09:28:39 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+function mapToNegativize(array) {
+    const negativeArray = []
+    for(const num of array) {
+        if (num.toString().charAt(0) === "-") {
+            negativeArray.push(Math.abs(num))
+        } else {
+            negativeArray.push(-Math.abs(num))
+        }
+    }
+    return negativeArray;
+}
+
+function mapToNoChange(array) {
+    return array
+}
+
+function mapToDouble(array) {
+    const doublesArray = []
+    for(const num of array) {
+        doublesArray.push(num * 2)
+    }
+    return doublesArray;
+}
+
+function mapToSquare(array) {
+    const squaresArray = []
+    for(const num of array) {
+        squaresArray.push(num ** 2)
+    }
+    return squaresArray;
+}
+
+function reduceToTotal(array, total=0) {
+
+    for(const num of array) {
+        total += num
+    }
+    return total;
+}
+
+function reduceToAllTrue(array) {
+    for(const item of array) {
+        if (!!item === false) {
+            return false;
+        }
+    }
+    return true;    
+}
+
+function reduceToAnyTrue(array) {
+    for(const item of array) {
+        if (!!item === true) {
+            return true;
+        }
+    }
+    return false;
+}
